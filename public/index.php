4ad218632397db3109fc2579416e222d9ff8ebd0
Brian Weaver <bdrums@gmail.com>
2021-08-02 12:21:22 -0400

    Use DI to handle the settings we'll need
    Slim can optionally make use of a [Dependency Injection
    Container](https://www.slimframework.com/docs/v4/concepts/di.html) to
    make resolving application dependencies easy. Add
    [php-di](https://php-di.org/), and then make the settings we'll need to
    make API requests available in it.

--- public/index.php
+++ public/index.php
@@ -3,13 +3,21 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+$container->set("OAUTH_APP_ID", getenv("OAUTH_APP_ID"));
+$container->set("OAUTH_SECRET", getenv("OAUTH_SECRET"));
+$container->set("SCOPE", "people services");
+$container->set("API_URL", "https://api.planningcenteronline.com");
+$container->set("TOKEN_EXPIRATION_PADDING", 300); /* go ahead and refresh a token if it's within this many seconds of expiring */
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("<h1>Hello world!</h1>");
+    $response->getBody()->write("<h1>Hello world!</h1>" . $this->get("SCOPE"));
     return $response;
 });
 
