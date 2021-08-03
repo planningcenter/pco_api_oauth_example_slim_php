b2b67720a752d48de876b35d46609ea79c781572
Brian Weaver <bdrums@gmail.com>
2021-08-03 13:32:52 -0400

    Add session management

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use DI\Container;
+use SlimSession\Helper as SessionHelper;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -13,14 +14,28 @@ $container->set("OAUTH_SECRET", getenv("OAUTH_SECRET"));
 $container->set("SCOPE", "people services");
 $container->set("API_URL", "https://api.planningcenteronline.com");
 $container->set("TOKEN_EXPIRATION_PADDING", 300); /* go ahead and refresh a token if it's within this many seconds of expiring */
+$container->set("session", function () {
+    return new SessionHelper();
+});
 AppFactory::setContainer($container);
 $app = AppFactory::create();
+$app->add(
+    new \Slim\Middleware\Session([
+        "name" => "pco_api_session",
+        "autorefresh" => true,
+        "lifetime" => "4 hours",
+    ])
+);
 
 $app->get('/', function (Request $request, Response $response, $args) {
+    $session = $this->get("session");
     // If we have a token to make requests
+    if ($session->exists("token")) {
         // Check if we need to refresh it
         // Fetch some people from the Planning Center API
-    // Otherwise, show a link to /auth to login with Planning Center
+    } else {
+        // Otherwise, show a link to /auth to login with Planning Center
+    }
 
     $response->getBody()->write("<h1>Hello world!</h1>" . $this->get("SCOPE"));
     return $response;
